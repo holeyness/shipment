53f4cc64e09067ee79b47f2ae975d00fd0386c6f
Ian Luo <ian@ianluo.com>
2017-06-09 14:28:31 -0400

    Initial Commit

---
+++ shipment/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  shipment
+//
+//  Created by Ian Luo on 6/9/17.
+//  Copyright (c) 2017 ianluo. All rights reserved.
+//
+
+import Cocoa
+
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+    }
+
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+    }
+
+
+
+}
---
+++ shipment/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  shipment
+//
+//  Created by Ian Luo on 6/9/17.
+//  Copyright (c) 2017 ianluo. All rights reserved.
+//
+
+import Cocoa
+
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+    }
+
+
+    override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+    }
+
+
+
+}
